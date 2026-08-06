7d96b36c25378f0f975db225a4dc8c19f7009848
abhishekabhi123 <vadakkemadamabhishek@gmail.com>
2026-08-06 15:36:25 +0530

    feat: initialize Prisma ORM with PostgreSQL and implement User repository

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,21 @@
+import type { Prisma, User } from "../generated/prisma/client.js";
+import prisma from "../config/database.js";
+
+
+export const userRepository = {
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+        return prisma.user.create({ data });
+    },
+
+    async findByEmail(email: string): Promise<User> {
+        return prisma.user.findUnique({
+            where: { email }
+        });
+    },
+
+    async findById(id: string): Promise<User | null> {
+        return prisma.user.findUnique({
+            where: { id }
+        })
+    }
+}
